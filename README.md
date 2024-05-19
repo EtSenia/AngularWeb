88a112e187bb4821808707bc775f1d268e8b7c35
Et <guidoyair@hotmail.com>
2024-05-18 23:34:33 -0500

    alv

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularWeb
+# angularweb
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.7.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AngularWeb' title`, () => {
+  it(`should have the 'angularweb' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularWeb');
+    expect(app.title).toEqual('angularweb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularWeb');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angularweb');
   });
 });
