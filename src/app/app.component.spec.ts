241b230e7fbee1fc10efb129443264299534ac47
Et <guidoyair@hotmail.com>
2024-05-18 23:25:43 -0500

    a

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angularproyect' title`, () => {
+  it(`should have the 'AngularWeb' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularproyect');
+    expect(app.title).toEqual('AngularWeb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angularproyect');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularWeb');
   });
 });
